9c71a575317b9b8cb295a3db5034948df28992a7
Genji Noguchi <genji.noguchi@gmail.com>
2014-03-24 01:00:58 -0400

    Finalized MyLinkedList Generic Implementation

--- APCS/DataStructures/Driver.java
+++ APCS/DataStructures/Driver.java
@@ -1,13 +1,19 @@
 public class Driver{
 	
 	public static void main(String[] args) {
-		MyLinkedList list = new MyLinkedList();
-		list.add("Hello", 0);
-		list.add("Goodbye", 0);
-		list.add("First",0);
+		MyLinkedList<Integer> list = new MyLinkedList<Integer>();
 		System.out.println(list);
 
-		System.out.println(list.find("Goodbye"));
+		list.add(new Integer(1), 0);
+		list.add(new Integer(2), list.length());
+		list.add(new Integer(3), 1);
+		list.remove(list.length()-1);
+
+		System.out.println("length: " + list.length());
+		System.out.println(list);
+		System.out.println();
+
+
 	}
 
 }
