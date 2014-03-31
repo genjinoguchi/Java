dbefacc214a24a6e78cea07c6e7872ab9d9868b7
Genji Noguchi <genji.noguchi@gmail.com>
2014-03-31 02:01:55 -0400

    Finished Polly.

--- StJosephs/Polly.java
+++ StJosephs/Polly.java
@@ -3,22 +3,16 @@ import java.io.File;
 
 public class Polly{
 	private int day;
-	ArrayList<PossibleLessonSequence>possible;
-	String input, output;
 
-	public String scanPolly(String filename){
+	public static String scanPolly(String filename){
 		try{
-			Scanner sc = new Scanner(new File(filename));
+			Scanner sc = new Scanner(new File(filename)).useDelimiter("\n");
 			int day = sc.nextInt();
-			for(int x=0;x<day;x++){
-				input = sc.nextLine();
-				output = "";
-				possible.clear();
-				findPermutations(input);
-				System.out.println(x);
-				System.out.println(possible);
+			String input = "",output = "";
+			while(sc.hasNext()){
+				input = sc.next();
+				output += findPermutations(input);
 			}
-
 			sc.close();
 			return output;
 		}catch(Exception e){
@@ -27,41 +21,31 @@ public class Polly{
 		}
 		return "";
 	}
-	public  matchAndRemove(PossibleLessonSequence s, int sindex){//Count duplicates of s and add them to s.
-		int total = 0;
-		for(int x=sindex-1;x>=0;x--){
-			if(possible.get(x).compareTo(s)==0){
-				possible.remove(x);
-				total++;
-			}
-		}
-
-
-		
-	}
-	public void findPermutations(String s){
+	public static String findPermutations(String s){
 		//Only look for adjacent strings. Set length and starting point and look for multiples.
-		for(int end = s.length();end>=0;end--){
-			
-		}
-	}
-
-	private class PossibleLessonSequence implements Comparable<PossibleLessonSequence>{
-		private String lesson;
-		private int occurrences = 1;
-
-		public PossibleLessonSequence(String s){
-			lesson = s;
-		}
-		public int compareTo(PossibleLessonSequence p){
-			if(p.lesson.equals(lesson)){
-				return 0;
-			}else{
-				return -1;
+		for(int length=1;length<=s.length()/2;length++){
+			String s1 = s.substring(s.length()-length, s.length());
+			String s2 = s.substring(s.length()-(2*length),s.length()-length);
+			if(s1.equals(s2)){
+				return s1 + " " + countPermutations(s,s1)+"\n";
 			}
 		}
-
+		return "Polly had a bad day\n";
 	}
-
-
+	public static int countPermutations(String s, String s1){
+		int total = 0;
+		int length = s1.length();
+		while(s.length()-(total+1)*length >= 0){
+			String s2 = s.substring(s.length()-(total+1)*length,s.length()-(total)*length);
+			if(s1.equals(s2)){
+				total++;
+			}else{
+				break;
+			}
+		}
+		return s.length()-(total)*length;
+	}
+	public static void main(String[] args) {
+		System.out.println(scanPolly("PollyIn.txt"));
+	}
 }
