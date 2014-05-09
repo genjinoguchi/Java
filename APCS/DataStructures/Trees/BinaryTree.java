866f94a7a9489375a933cf849cb9cd7e55f2466f
Genji Noguchi <genji.noguchi@gmail.com>
2014-05-08 21:24:58 -0400

    Finished all of Binary Trees except that the toString method still spits out too many new lines.

--- APCS/DataStructures/Trees/BinaryTree.java
+++ APCS/DataStructures/Trees/BinaryTree.java
@@ -19,7 +19,11 @@ public class BinaryTree{
 
 		System.out.println(this);
 
-		System.out.println(find(15));
+		System.out.println("////////////////////////////////////////////////");
+
+		System.out.println(remove(65));
+
+		System.out.println(this);
 
 	}
 
@@ -32,14 +36,14 @@ public class BinaryTree{
 				if(temp.getValue()==value){
 					temp.increaseCounter();
 					return;
-				}else if(temp.getValue()>value){
+				}else if(temp.getValue()<value){
 					if(temp.getRight()==null){
 						temp.setRight(new Node(value));
 						return;
 					}else{
 						temp = temp.getRight();
 					}
-				}else if(temp.getValue()<value){
+				}else if(temp.getValue()>value){
 					if(temp.getLeft()==null){
 						temp.setLeft(new Node(value));
 						return;
@@ -52,33 +56,90 @@ public class BinaryTree{
 	}
 
 	public boolean remove(int value){
-		Node temp = find(value);
-		if(temp==null){ return false; }
-		Node replacement=new Node(value);
-		if(temp.getRight()!=null){
-			replacement = getMaxChild(Node n);
-		}else if(temp.getLeft()!=null){
-			replacement = getMinChild(Node n);
-		}else{
-			
+		if(root.getValue()==value){
+			if(root.getRight()!=null){
+				Node temp = getAndRemoveMinChild(root.getRight());
+				temp.setRight(root.getRight());
+				temp.setLeft(root.getLeft());
+				root = temp;
+			}else if(root.getLeft()!=null){
+				Node temp = getAndRemoveMaxChild(root.getLeft());
+				temp.setRight(root.getRight());
+				temp.setLeft(root.getLeft());
+				root = temp;
+			}else{
+				root = null;
+			}
+			return true;
+		}else if(find(value)==null){
+			System.out.println("Massive hur");
+			return false;
 		}
+		return remove(root, value);
 	}
 
-	public Node getMaxChild(Node n){
+	public boolean remove(Node n, int value){
+		if(n==null){
+			System.out.println("The hurr");
+			return false;
+		}
+		else if(n.getValue()==value){
+			System.out.println("Hurr genji screwed up");
+			return false;
+		}else if(n.getValue()<value){
+			if(n.getRight().getValue()==value){
+				Node temp = n.getRight();
+				n.setRight(getAndRemoveMinChild(n.getRight()));
+				if(n.getRight()!=null){
+					n.getRight().setRight(temp.getRight());
+					n.getRight().setLeft(temp.getLeft());
+				}
+				return true;
+			}else{
+				return remove(n.getRight(),value);
+			}
+		}else if(n.getValue()>value){
+			if(n.getLeft().getValue()==value){
+				Node temp = n.getLeft();
+				n.setLeft(getAndRemoveMaxChild(n.getLeft()));
+				if(n.getLeft()!=null){	
+					n.getLeft().setRight(temp.getRight());
+					n.getLeft().setLeft(temp.getLeft());
+				}
+				return true;
+			}else{
+				return remove(n.getLeft(),value);
+			}
+
+		}
+		System.out.println("What the hurr");
+		return false;
+	}
+
+	public Node getAndRemoveMaxChild(Node n){ //get and remove. Assuming n has a right child.
 		Node temp = n;
-		while(temp.getRight()!=null){
+		if(n.getRight()==null){
+			return null;
+		}
+		while(temp.getRight().getRight()!=null){
 			temp = temp.getRight();
-		}
-		return temp;
+		} //Now temp should be the parent of the max.
+		Node max = temp.getRight();
+		temp.setRight(null);
+		return max;
 	}
 
-	public Node getMinChild(Node n){
+	public Node getAndRemoveMinChild(Node n){ //Assuming n has a left child.
 		Node temp = n;
-		while(temp.getLeft()!=null){
-			temp = temp.getLeft();
-		}
-		return temp;
+		if(n.getLeft()==null){ return null; }
+		while(temp.getLeft().getLeft()!=null){
+			temp=temp.getLeft();
+		} //Temp should now be the parent of the min.
+		Node min = temp.getLeft();
+		temp.setLeft(null);
+		return min;
 	}
+
 	public Node find(int value){
 		Node temp = root;
 		if(root==null){
@@ -87,9 +148,9 @@ public class BinaryTree{
 			while(temp!=null){
 				if(temp.getValue()==value){
 					break;
-				}else if(temp.getValue()>value){
-					temp = temp.getRight();
 				}else if(temp.getValue()<value){
+					temp = temp.getRight();
+				}else if(temp.getValue()>value){
 					temp = temp.getLeft();
 				}
 			}
@@ -99,7 +160,11 @@ public class BinaryTree{
 
 	public String toString(){
 		if(root==null){return "Empty tree.";}
-		return toString(root);
+		String toReturn = "";
+		for(String s : toString(root).split("\\n")){
+			toReturn+=s+"\n";	
+		}
+		return toReturn;
 	}
 	public String toString(Node n){
 		String toReturn = n.toString();
