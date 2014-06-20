32100f14eaab13892065551518383f4e85c525fa
Genji Noguchi <genji.noguchi@gmail.com>
2014-06-20 11:46:11 -0400

    Switching computers so I'm committing.

---
+++ DataStructurePractice/Notepad/ActionManager.java
@@ -0,0 +1,38 @@
+
+public class ActionManager{
+	private ActionSequence actionSequence;
+
+	public ActionManager(){
+		actionSequence = new ActionSequence();
+
+	}
+
+	public void addUndoableAction(){}
+
+	private class ActionSequence{
+		private Node<UndoableAction>
+
+
+		// Combination of stack and linked list
+		private class Node<E>{
+			private Node<E> forward,backward;
+			private E data;
+
+			public Node(E data){
+				this.data = data;
+			}
+			public Node<E> getFront(){
+				return forward;
+			}
+			public Node<E> getBack(){
+				return backward;
+			}
+			public E getData(){
+				return data;
+			}
+
+		}
+	}
+
+
+}
